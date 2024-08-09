3b64cfd79050a5d7666b8cc779c13621c2098c37
joselacunzarw <joselacunzarw@gmail.com>
2024-08-09 13:26:36 -0300

    con open ia

--- a_env_vars.py
+++ a_env_vars.py
@@ -1,3 +1,4 @@
-EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
+EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"#"all-MiniLM-L6-v2"
 DATA_PATH = r"C:\Users\josel\OneDrive\Documents\Desktop\repo"
 CHROMA_PATH = "../chroma"
+OPENAI_API_KEY = ''
--- core.py
+++ core.py
@@ -1,12 +1,17 @@
 # Dependencias de Langchain
+from datetime import datetime
 from langchain_huggingface import HuggingFaceEmbeddings  # Importar embeddings de HuggingFace de Langchain
-from langchain_community.document_loaders import PyPDFDirectoryLoader  # Importar cargador de PDF de Langchain
+from langchain_community.document_loaders import PyPDFDirectoryLoader, DirectoryLoader  # Importar cargador de PDF de Langchain
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter  # Importar el cortador de texto de Langchain
 from langchain.schema import Document  # Importar el esquema de Documento de Langchain
 from langchain_community.vectorstores import Chroma  # DB vectorial Chroma de Langchain
 import os  # Importar módulo os para funcionalidades del sistema operativo
 import shutil  # Importar módulo shutil para operaciones de archivos de alto nivel
 import a_env_vars  # Importar módulo para manejar variables de entorno
+from langchain_openai import OpenAIEmbeddings
+#from langchain_ollama import OllamaEmbeddings
+
 
 # Variables globales
 EMBEDDING_MODEL_NAME =  a_env_vars.EMBEDDING_MODEL_NAME
@@ -16,6 +21,7 @@ CHROMA_PATH = a_env_vars.CHROMA_PATH
 # Constante para el tamaño máximo del lote
 MAX_BATCH_SIZE = 5461
 
+os.environ["OPENAI_API_KEY"] = a_env_vars.OPENAI_API_KEY
 
 
 def load_documents() -> list[Document]:
@@ -25,7 +31,9 @@ def load_documents() -> list[Document]:
         Lista de Documentos: Documentos PDF cargados representados como objetos Document de Langchain.
     """
     # Inicializar el cargador de PDF con el directorio especificado
-    document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    #document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    document_loader = DirectoryLoader(DATA_PATH, glob="**/*")
+    
     # Cargar los documentos PDF y retornarlos como una lista de objetos Document
     print ("Documentos Leidos")
     return document_loader.load()
@@ -54,7 +62,7 @@ def split_text(documents: list[Document]) -> list[Document]:
     # Imprimir ejemplo de contenido de página y metadatos para un fragmento
     if chunks:
         document = chunks[0]
-        print(document.page_content)
+       # print(document.page_content)
         print(document.metadata)
 
     return chunks  # Retornar la lista de fragmentos de texto divididos
@@ -71,7 +79,9 @@ def save_to_chroma(chunks):
         shutil.rmtree(CHROMA_PATH)
 
     # Crear una nueva base de datos Chroma a partir de los documentos utilizando embeddings de HuggingFace
-    embedding_function = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
+    #embedding_function = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
+    embedding_function = OpenAIEmbeddings()
+    #embedding_function = OllamaEmbeddings(model="llama3.1:8b")
     
     # Procesar los fragmentos en lotes más pequeños
     for i in range(0, len(chunks), MAX_BATCH_SIZE):
@@ -88,10 +98,15 @@ def generate_data_store():
     """
     Función para generar una base de datos vectorial en Chroma a partir de documentos.
     """
+    print ("Buscando Documentos"  + str(datetime.now()))
     documents = load_documents()  # Cargar documentos desde una fuente
-    for doc in documents:
-        print(doc.metadata)
+    print ("documentos cargados "  + str(datetime.now()))
+    print ("inicio de CHUNKING "  + str(datetime.now()))
     chunks = split_text(documents)  # Dividir los documentos en fragmentos manejables
+    print ("fin CHUNK "  + str(datetime.now()))
+    print ("Guardar en DB "  + str(datetime.now()))
     save_to_chroma(chunks)  # Guardar los datos procesados en un almacén de datos
+    print ("Fin Guardar en DB "  + str(datetime.now()))
+
 
 generate_data_store()
